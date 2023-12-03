bbcad6928c51a57742acc435505c9af2c437da6c
florianlangard <florian.langard@gmail.com>
2023-12-03 16:38:50 +0100

    end day1 - start day2

--- day1/advent.php
+++ day1/advent.php
@@ -1,28 +1,51 @@
 <?php
 
-$raw = file_get_contents('data.txt');
+$raw = file_get_contents('./data.txt');
 // $data = str_replace("\n\n"," ",$raw);
 $data = explode("\n", $raw);
 
 
 //= Part One =========================================
 
-$i = 0;
+// $i = 0;
+// foreach ($data as $string) {
+
+//     $tab[$i] = "";
+//     preg_match('/\d/', $string, $matches);
+//     $tab[$i] .= $matches[0];
+
+//     $string = strrev($string);
+    
+//     preg_match('/\d/', $string, $matches);
+//     $tab[$i] .= $matches[0];
+
+//     $i++;
+// }
+
+// $result1 = array_sum($tab);
+
+//= Part Two =========================================
+
+$haystack = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+$replace = ["one1one", "two2two", "three3three", "four4four", "five5five", "six6six", "seven7seven", "eight8eight", "nine9nine"];
+
+$glob = [];
+
 foreach ($data as $string) {
-    
-    $tab[$i] = "";
-    preg_match('/\d/', $string, $matches);
-    $tab[$i] .= $matches[0];
+    $mid = str_replace($haystack, $replace, $string);
+    $res = preg_replace("/[^0-9]+/", "", $mid);
 
-    $string = strrev($string);
-    
-    preg_match('/\d/', $string, $matches);
-    $tab[$i] .= $matches[0];
+    $first = substr($res, 0, 1);
+    $last = substr($res, -1, 1);
 
-    $i++;
+    $num = $first . $last;
+    $glob[] = $num;
 }
 
-$result = array_sum($tab);
+$result = array_sum($glob);
+
+
+
 
 echo "<pre>";
 print_r($result);
---
+++ day2/advent.php
@@ -0,0 +1,21 @@
+<?php
+
+$raw = file_get_contents('./sample.txt');
+// $data = str_replace("\n\n"," ",$raw);
+$data = explode("\n", $raw);
+
+
+//= Part One =========================================
+
+
+
+//= Part Two =========================================
+
+
+
+$res = "maybe one day";
+
+echo "<pre>";
+print_r($data);
+echo "</pre>";
+
