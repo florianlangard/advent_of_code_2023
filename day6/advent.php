73759cb5b0fb202c75c2e9817d0c9be1b3b138e0
florianlangard <florian.langard@gmail.com>
2023-12-15 22:46:15 +0100

    day6 part2 done, but slow

--- day6/advent.php
+++ day6/advent.php
@@ -8,40 +8,56 @@ $res = "oh";
 
 //= Part One =========================================
 
-$timeInter = explode(":", $data[0]);
-$trim = preg_replace('/\s+/', " ", $timeInter[1]);
-$timeTab = explode(" ", $trim);
-$timeTab = array_filter($timeTab);
+// $timeInter = explode(":", $data[0]);
+// $trim = preg_replace('/\s+/', " ", $timeInter[1]);
+// $timeTab = explode(" ", $trim);
+// $timeTab = array_filter($timeTab);
 
-$distanceInter = explode(":", $data[1]);
-$trim = preg_replace('/\s+/', " ", $distanceInter[1]);
-$distanceTab = explode(" ", $trim);
-$distanceTab = array_filter($distanceTab);
+// $distanceInter = explode(":", $data[1]);
+// $trim = preg_replace('/\s+/', " ", $distanceInter[1]);
+// $distanceTab = explode(" ", $trim);
+// $distanceTab = array_filter($distanceTab);
 
 
 
-for ($j=1; $j <= count($timeTab) ; $j++) { 
+// for ($j=1; $j <= count($timeTab) ; $j++) { 
 
-    $nbWays[$j] = 0;
+//     $nbWays[$j] = 0;
 
-    for ($i=0; $i < $timeTab[$j]; $i++) { 
-        $speed = $timeHold = $i;
-        $raceTime = $timeTab[$j] - $i;
-        $distance = $raceTime * $speed;
+//     for ($i=0; $i < $timeTab[$j]; $i++) { 
+//         $speed = $timeHold = $i;
+//         $raceTime = $timeTab[$j] - $i;
+//         $distance = $raceTime * $speed;
         
-        if ($distance > $distanceTab[$j]) {
-            $nbWays[$j]++;
-        }
-    }
-}
+//         if ($distance > $distanceTab[$j]) {
+//             $nbWays[$j]++;
+//         }
+//     }
+// }
 
-$res = array_product($nbWays);
+// $res = array_product($nbWays);
 
 //= Part Two =========================================
 
+$timeInter = explode(":", $data[0]);
+$trimTime = str_replace(' ', '', $timeInter[1]);
 
+$distanceInter = explode(":", $data[1]);
+$trimDist = str_replace(' ', "", $distanceInter[1]);
+
+$nbWays = 0;
+
+for ($i=0; $i < $trimTime; $i++) { 
+    $speed = $timeHold = $i;
+    $raceTime = $trimTime - $i;
+    $distance = $raceTime * $speed;
+    
+    if ($distance > $trimDist) {
+        $nbWays++;
+    }
+}
 
 echo "<pre>";
-print_r($res);
+print_r($nbWays);
 echo "</pre>";
 
